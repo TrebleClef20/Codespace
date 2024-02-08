29c7f31aa5d05b3abb59da038d5545c377ab4146
TrebleClef20 <65029347+TrebleClef20@users.noreply.github.com>
2024-02-08 00:48:22 +0000

    init

--- Character.java
+++ Character.java
@@ -1,30 +1,7 @@
 public class Character {
-    private String type;
+    private Type type;
 
-    public Character(String type) {
+    public Character(Type type) {
         this.type = type;
-    }
-
-    public void attack() {
-        if (type.equals("Knight")) {
-            System.out.println("Knight attacks with a sword!");
-        } else if (type.equals("Wizard")) {
-            System.out.println("Wizard casts a spell!");
-        } else if (type.equals("Archer")) {
-            System.out.println("Archer shoots an arrow!");
-        }
-    }
-
-    public void defend() {
-        if (type.equals("Knight")) {
-            System.out.println("Using a shield to defend!");
-            System.out.println("Dodgin to avoid attack!");
-            System.out.println("Creating a magic barrier for defense!");
-
-        } else if (type.equals("Wizard")) {
-            System.out.println("Creating a magic barrier for defense!");
-        } else if (type.equals("Archer")) {
-            System.out.println("Using a shield to defend!");
-        }
     }
 }
