2010076c7b56afa3255cb5caacbf26d1b0e6b0d0
TrebleClef20 <65029347+TrebleClef20@users.noreply.github.com>
2024-02-08 00:37:03 +0000

    init

--- GodObject_Sample/Order.java
+++ GodObject_Sample/Order.java
@@ -1,21 +1,21 @@
 package GodObject_Sample;
+
 public class Order {
     private Double orderAmount;
     private String paymentType;
 
-    public Order (Double orderAmount, String paymentType){
+    public Order(Double orderAmount, String paymentType) {
         this.orderAmount = orderAmount;
         this.paymentType = paymentType;
     }
 
     // Three types of payment: Gcash, Maya, Cash
-    public String calculatePayment(){
+    public String calculatePayment() {
         String output = "";
         if (paymentType.equals("GCash")) {
             output += "Payment is GCash\n";
             output += "Energy for trees: 10 points";
-        } 
-        else if (paymentType.equals("Maya")){
+        } else if (paymentType.equals("Maya")) {
             output += "Payment is Maya\n";
             output += "You will receive additional 1% interest on Savings";
         } else {
@@ -23,7 +23,7 @@ public class Order {
             output += "No incentives";
         }
 
-        return "Your order amount is " + orderAmount + 
-                "\n" + output; 
+        return "Your order amount is " + orderAmount +
+                "\n" + output;
     }
 }
